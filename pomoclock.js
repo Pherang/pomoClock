b16f2659bb80b944a933242868f7953cbe0e591c
unknown <Pherang@users.noreply.github.com>
2018-02-27 07:26:48 -0500

    More pseudo code for program

--- pomoclock.js
+++ pomoclock.js
@@ -2,7 +2,7 @@
 * Need something to track the Break Length and the Session Length
 * The session length counts down until it ends then the break length starts and counts down until it ends.
 * This keeps switching back and forward until user stops using the clock.
-* It looks like the Date object could be useful here.
+* It looks like the Date object could be useful here as well as setInterval() or setTimeout()
 */
 
 /*
