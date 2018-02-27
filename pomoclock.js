2b10a633f87b1665244835640c8d4c694dffc9cd
unknown <Pherang@users.noreply.github.com>
2018-02-27 05:55:18 -0500

    Added comments to structure the program.

---
+++ pomoclock.js
@@ -0,0 +1,37 @@
+/*
+* Need something to track the Break Length and the Session Length
+* The session length counts down until it ends then the break length starts and counts down until it ends.
+* This keeps switching back and forward until user stops using the clock.
+* It looks like the Date object could be useful here.
+*/
+
+/*
+* Session timer
+*
+*/
+
+
+
+
+
+
+
+
+/*
+* Break timer
+*
+*/
+
+
+
+
+
+
+
+
+
+/*
+* Switcher
+*
+*
+*/
