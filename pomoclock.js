54dfeb742baf3618383c124dcbf2f5c96e4931a8
pherang <pherang@users.noreply.github.com>
2018-02-27 19:54:15 -0500

    Added modules for the break and session timer.

--- pomoclock.js
+++ pomoclock.js
@@ -3,6 +3,7 @@
 * The session length counts down until it ends then the break length starts and counts down until it ends.
 * This keeps switching back and forward until user stops using the clock.
 * It looks like the Date object could be useful here as well as setTimeout()
+* Upon further reading setTimeout may not be best. requestionAnimationFrame() might be though.
 */
 
 /*
@@ -10,13 +11,46 @@
 * Should be able to increase and decrease by one minute.
 * Default session time is 25 minutes.
 */
+var sessionTime = ( function() {
+    var counter = 25;
+    function changeCounter(val) {
+        counter += val;
+    } // Return an object with the key value pairs.
+    return {
+        increment: function() {
+            changeCounter(1);
+        },
+        decrement: function() {
+            changeCounter(-1);
+        },
+        value: function() {
+            return counter;
+        }
+    };
+})();   
 
 /*
 * Break timer
 * Should be able to increase and decrease by one minute.
 * Default break time is 5 minutes.
 */
-
+var breakTime = ( function() {
+    var counter = 5;
+    function changeCounter(val) {
+        counter += val;
+    } // Return an object with the key value pairs.
+    return {
+        increment: function() {
+            changeCounter(1);
+        },
+        decrement: function() {
+            changeCounter(-1);
+        },
+        value: function() {
+            return counter;
+        }
+    };
+})();   
 /*
 * Switcher
 * Switches what is displayed on the timer. Either the break time or the session time depending on which one has run out.
@@ -29,3 +63,17 @@
 *
 */
 
+
+
+window.requestAnimationFrame(countDown);
+
+
+
+
+// Playing with timers
+sessionTime.increment();
+console.log(sessionTime.value());
+console.log("test");
+
+breakTime.decrement();
+console.log(breakTime.value());
